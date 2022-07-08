59d42a3592ab8b09c28c6464eae0283e37e3e56f
DongSu Kim <dskim9882@gmail.com>
2022-07-08 23:38:04 +0900

    Make Layout of Growth Setting Screen

---
+++ app/src/main/java/com/uniroad/kiduck/GrowthSettingActivity.kt
@@ -0,0 +1,11 @@
+package com.uniroad.kiduck
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class GrowthSettingActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_growth_setting)
+    }
+}
--- app/src/main/java/com/uniroad/kiduck/SettingsActivity.kt
+++ app/src/main/java/com/uniroad/kiduck/SettingsActivity.kt
@@ -3,6 +3,7 @@ package com.uniroad.kiduck
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.uniroad.kiduck.databinding.ActivitySettingsBinding
+import org.jetbrains.anko.startActivity
 
 class SettingsActivity : AppCompatActivity() {
     private lateinit var binding: ActivitySettingsBinding
@@ -10,6 +11,22 @@ class SettingsActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivitySettingsBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.gotoGrowthSetting.setOnClickListener {
+            startActivity<GrowthSettingActivity>()
+        }
+
+        binding.gotoNameSetting.setOnClickListener {
+            startActivity<ChangeNameActivity>()
+        }
+
+        binding.gotoPasswordSetting.setOnClickListener {
+            startActivity<ChangePasswordActivity>()
+        }
+
+        binding.gotoEmergencyAlarm.setOnClickListener {
+            startActivity<EmergencyAlarmActivity>()
+        }
 
 
     }
