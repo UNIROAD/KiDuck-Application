32e45e45ed35b0f38d6e75e59bea69a735d62ab1
DongSu Kim <dskim9882@gmail.com>
2022-07-10 02:11:25 +0900

    Test Making a Bar Chart

--- app/src/main/java/com/uniroad/kiduck/SummaryActivity.kt
+++ app/src/main/java/com/uniroad/kiduck/SummaryActivity.kt
@@ -14,172 +14,140 @@ import com.github.mikephil.charting.interfaces.datasets.IBarDataSet
 import com.uniroad.kiduck.databinding.ActivitySummaryBinding
 import org.jetbrains.anko.startActivity
 
+
+
 class SummaryActivity : AppCompatActivity() {
     private lateinit var binding: ActivitySummaryBinding
 
-    /*private fun setChartView(view: View) {
-        var chartWeek = view.findViewById<BarChart>(R.id.chart_week)
-        setWeek(chartWeek)
-    }
+    /*
+    키덕으로부터 받아야 하는 데이터
+        이름
+        데이터의 갯수
 
-    private fun initBarDataSet(barDataSet: BarDataSet) {
-        //Changing the color of the bar
-        barDataSet.color = Color.parseColor("#304567")
-        //Setting the size of the form in the legend
-        barDataSet.formSize = 15f
-        //showing the value of the bar, default true if not set
-        barDataSet.setDrawValues(false)
-        //setting the text size of the value of the bar
-        barDataSet.valueTextSize = 12f
-    }
+        걸음수
+        음수량
+        통신 횟수
 
-    private fun setWeek(barChart: BarChart) {
-        initBarChart(barChart)
+        성장 조건별 기준
+            하루당 걸음수 기준
+            하루당 음수량 기준
+            하루당 통신 횟수 기준
 
-        barChart.setScaleEnabled(false) //Zoom In/Out
+        암호
 
-        val valueList = ArrayList<Double>()
-        val entries: ArrayList<BarEntry> = ArrayList()
-        val title = "걸음 수"
+        응급 알람 활성화 상태
+     */
 
-        //input data
-        for (i in 0..5) {
-            valueList.add(i * 100.1)
-        }
-
-        //fit the data into a bar
-        for (i in 0 until valueList.size) {
-            val barEntry = BarEntry(i.toFloat(), valueList[i].toFloat())
-            entries.add(barEntry)
-        }
-        val barDataSet = BarDataSet(entries, title)
-        val data = BarData(barDataSet)
-        barChart.data = data
-        barChart.invalidate()
-    }
-
-    private fun initBarChart(barChart: BarChart) {
-        //hiding the grey background of the chart, default false if not set
-        barChart.setDrawGridBackground(false)
-        //remove the bar shadow, default false if not set
-        barChart.setDrawBarShadow(false)
-        //remove border of the chart, default false if not set
-        barChart.setDrawBorders(false)
-
-        //remove the description label text located at the lower right corner
-        val description = Description()
-        description.setEnabled(false)
-        barChart.setDescription(description)
-
-        //X, Y 바의 애니메이션 효과
-        barChart.animateY(1000)
-        barChart.animateX(1000)
-
-
-        //바텀 좌표 값
-        val xAxis: XAxis = barChart.getXAxis()
-        //change the position of x-axis to the bottom
-        xAxis.position = XAxis.XAxisPosition.BOTTOM
-        //set the horizontal distance of the grid line
-        xAxis.granularity = 1f
-        xAxis.textColor = Color.RED
-        //hiding the x-axis line, default true if not set
-        xAxis.setDrawAxisLine(false)
-        //hiding the vertical grid lines, default true if not set
-        xAxis.setDrawGridLines(false)
-
-
-        //좌측 값 hiding the left y-axis line, default true if not set
-        val leftAxis: YAxis = barChart.getAxisLeft()
-        leftAxis.setDrawAxisLine(false)
-        leftAxis.textColor = Color.RED
-
-
-        //우측 값 hiding the right y-axis line, default true if not set
-        val rightAxis: YAxis = barChart.getAxisRight()
-        rightAxis.setDrawAxisLine(false)
-        rightAxis.textColor = Color.RED
-
-
-        //바차트의 타이틀
-        val legend: Legend = barChart.getLegend()
-        //setting the shape of the legend form to line, default square shape
-        legend.form = Legend.LegendForm.LINE
-        //setting the text size of the legend
-        legend.textSize = 11f
-        legend.textColor = Color.YELLOW
-        //setting the alignment of legend toward the chart
-        legend.verticalAlignment = Legend.LegendVerticalAlignment.TOP
-        legend.horizontalAlignment = Legend.LegendHorizontalAlignment.CENTER
-        //setting the stacking direction of legend
-        legend.orientation = Legend.LegendOrientation.HORIZONTAL
-        //setting the location of legend outside the chart, default false if not set
-        legend.setDrawInside(false)
-    }*/
+    private lateinit var kiduckName: String
+    private lateinit var dataCount: String
+    private lateinit var numOfSteps: ArrayList<String>
+    private lateinit var amountOfDrink: ArrayList<String>
+    private lateinit var numOfCommunication: ArrayList<String>
+    private lateinit var criteriaOfSteps: String
+    private lateinit var criteriaOfDrink: String
+    private lateinit var criteriaOfCommunication: String
+    private lateinit var kiduckPassword: String
+    private lateinit var emergencyAlarm_isEnabled: String
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivitySummaryBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        // 코드 작성 참고 링크 : https://stickode.tistory.com/127
-        var walkChart: BarChart = binding.chartWalk
+        // 블루투스 통신으로 KiDuck 데이터 수신
+        kiduckName = "DONGSU"
+        dataCount = "7"
 
-        val walk_entries = ArrayList<BarEntry>()
-        walk_entries.add(BarEntry(1f, 1000f))
-        walk_entries.add(BarEntry(2f, 3024f))
-        walk_entries.add(BarEntry(3f, 5921f))
-        walk_entries.add(BarEntry(4f, 1322f))
-        walk_entries.add(BarEntry(5f, 549f))
-        walk_entries.add(BarEntry(6f, 5976f))
-        walk_entries.add(BarEntry(7f, 7800f))
+        numOfSteps = ArrayList<String>()
+        numOfSteps.add("1000")
+        numOfSteps.add("424")
+        numOfSteps.add("5959")
+        numOfSteps.add("5894")
+        numOfSteps.add("7233")
+        numOfSteps.add("1294")
+        numOfSteps.add("12399")
+        amountOfDrink = ArrayList<String>()
 
-        walkChart.run {
-            description.isEnabled = false // 차트 옆에 별도로 표기되는 description을 안보이게 설정 (false)
-            setMaxVisibleValueCount(7) // 최대 보이는 그래프 개수를 7개로 지정
-            setPinchZoom(false) // 핀치줌(두손가락으로 줌인 줌 아웃하는것) 설정
-            setDrawBarShadow(false) //그래프의 그림자
-            setDrawGridBackground(false)//격자구조 넣을건지
+        numOfCommunication = ArrayList<String>()
+
+        val DATA_COUNT = dataCount.toInt()
+        val MAX_STEPS = 12399f
+
+        // 요약 내용 없데이트
+        binding.name.text = kiduckName + "의 기기"
+
+        // 차트 설정
+        var stepChart: BarChart = binding.chartWalk
+
+        val step_entries = ArrayList<BarEntry>()
+        for(i in 1..DATA_COUNT) {
+            step_entries.add(BarEntry(i.toFloat(), numOfSteps[i-1].toFloat()))
+        }
+
+        var step_set = BarDataSet(step_entries, "걸음수")
+            .apply {
+                color = Color.parseColor("#EB592A")
+                setDrawIcons(false)
+                setDrawValues(true)
+                valueFormatter = CustomDecimalFormatter()
+                valueTextColor = Color.BLACK
+            }
+
+        var step_dataSet = ArrayList<IBarDataSet>()
+        step_dataSet.add(step_set)
+
+        var step_data = BarData(step_dataSet)
+            .apply {
+                barWidth = 0.8f
+                setValueTextSize(10f)
+            }
+
+        stepChart.data = step_data
+
+        stepChart.apply {
+            description.isEnabled = false
             background = getDrawable(R.drawable.chart_body_background)
-            axisLeft.run { //왼쪽 축. 즉 Y방향 축을 뜻한다.
-                axisMaximum = 10001f //10000 위치에 선을 그리기 위해 10001f로 맥시멈값 설정
-                axisMinimum = 0f // 최소값 0
-                granularity = 1000f // 50 단위마다 선을 그리려고 설정.
-                setDrawLabels(true) // 값 적는거 허용 (0, 50, 100)
-                setDrawGridLines(true) //격자 라인 활용
-                setDrawAxisLine(false) // 축 그리기 설정
+            setPinchZoom(false)
+            setDrawBarShadow(false)
+            setDrawValueAboveBar(true)
+            setDrawGridBackground(false)
+
+            xAxis.apply {
+                position = XAxis.XAxisPosition.BOTTOM
+                granularity = 1f
+                setDrawGridLines(false)
+                setDrawAxisLine(true)
+                textSize = 12f
+                textColor = Color.parseColor("#A5A4A8")
+                valueFormatter = MyXAxisFormatter()
+            }
+
+            axisLeft.isEnabled = false
+
+            axisRight.apply {
+                axisMaximum = MAX_STEPS + 1f
+                axisMinimum = 0f
+                granularity = 1000f
+                setDrawLabels(true)
+                setDrawGridLines(true)
+                setDrawAxisLine(true)
                 axisLineColor = Color.parseColor("#A5A4A8") // 축 색깔 설정
                 gridColor = Color.parseColor("#A5A4A8") // 축 아닌 격자 색깔 설정
                 textColor = Color.parseColor("#A5A4A8") // 라벨 텍스트 컬러 설정
                 textSize = 13f //라벨 텍스트 크기
             }
-            xAxis.run {
-                position = XAxis.XAxisPosition.BOTTOM //X축을 아래에다가 둔다.
-                granularity = 1f // 1 단위만큼 간격 두기
-                setDrawAxisLine(true) // 축 그림
-                setDrawGridLines(false) // 격자
-                textColor = Color.parseColor("#A5A4A8") //라벨 색상
-                textSize = 12f // 텍스트 크기
-                valueFormatter = MyXAxisFormatter() // X축 라벨값(밑에 표시되는 글자) 바꿔주기 위해 설정
-            }
-            axisRight.isEnabled = false // 오른쪽 Y축을 안보이게 해줌.
-            setTouchEnabled(true) // 그래프 터치해도 아무 변화없게 막음
-            animateY(1000) // 밑에서부터 올라오는 애니매이션 적용
-            legend.isEnabled = false //차트 범례 설정
-        }
 
-        var set = BarDataSet(walk_entries,"DataSet") // 데이터셋 초기화
-        set.color = Color.parseColor("#EB592A") // 바 그래프 색 설정
+            setTouchEnabled(true)
+            animateY(1000)
+            legend.isEnabled = false
 
-        val dataSet :ArrayList<IBarDataSet> = ArrayList()
-        dataSet.add(set)
-        val data = BarData(dataSet)
-        data.barWidth = 0.8f //막대 너비 설정
-        walkChart.run {
-            this.data = data //차트의 데이터를 data로 설정해줌.
+            data = step_data
             setFitBars(true)
             invalidate()
         }
+
+
+
 
         binding.chartWalk.setOnClickListener {
             startActivity<StaticsActivity>()
@@ -197,6 +165,13 @@ class SummaryActivity : AppCompatActivity() {
         private val days = arrayOf("1차","2차","3차","4차","5차","6차","7차")
         override fun getAxisLabel(value: Float, axis: AxisBase?): String {
             return days.getOrNull(value.toInt()-1) ?: value.toString()
+        }
+    }
+
+    inner class CustomDecimalFormatter : ValueFormatter() {
+        override fun getFormattedValue(value: Float): String {
+            val score = value.toString().split(".")
+            return score[0]
         }
     }
 }
